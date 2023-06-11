940e18c2fedc64cb57ac8407798fec9634eac6e2
Alex <60247838+alexapostolu@users.noreply.github.com>
2023-06-10 23:11:29 -0400

    Update Bag.java

--- src/main/java/Bag.java
+++ src/main/java/Bag.java
@@ -13,6 +13,10 @@ public abstract class Bag {
      *       - an int named capacity
      *       - an array of Strings named contents
      */
+    private String color;
+    private int numberOfContents;
+    private int capacity;
+    private String[] contents;
 
 
 
@@ -26,6 +30,12 @@ public abstract class Bag {
      * be empty (e.g. numberOfContents is 0 and an empty String array for
      * its contents.)
      */
+    Bag(String _color, int _capacity)
+    {
+        color = _color;
+        capacity = _capacity;
+        contents = new String[capacity];
+    }
 
 
 
@@ -37,6 +47,9 @@ public abstract class Bag {
      *           - getNumberOfContents
      *           - getCapacity
      */
+    String getColor() { return color; }
+    String[] getNumberOfContents() { return contents.length; }
+    int getCapacity() { return capacity; }
 
 
 
@@ -45,6 +58,7 @@ public abstract class Bag {
      * TODO: Create a setter function called setColor which sets the
      *       color of this bag to the given color.
      */
+    void setColor(Color _color) { color = _color; }
 
 
 
@@ -60,6 +74,17 @@ public abstract class Bag {
      *       This method should return true if the item was added
      *       and false otherwise.
      */
+    boolean addItem(String _item)
+    {
+        if (numberOfContents < capacity)
+        {
+            contents[numberOfContents] = _item;
+            numberOfContents += 1;
+            return true;
+        }
+        
+        return false;
+    }
 
 
 
@@ -75,6 +100,12 @@ public abstract class Bag {
      *
      * @return
      */
+    String popItem()
+    {
+        if (numberOfContents == 0)
+            return null;
+        numberOfContents -= 1;
+    }
 
 
 
@@ -87,7 +118,11 @@ public abstract class Bag {
      */
     public void increaseCapacity(int n) {
         // TODO: Implement this method.
-
+        String[] new_arr = new String[capacity + n];
+        capacity += n;
+        for (int i = 0; i < numberOfContents; i += 1)
+            new_arr[i] = contents[i];
+        contents = new_arr;
     }
 
     /**
