595f5bb974e6972c84913c499ba487396e3c08c7
Alex <60247838+alexapostolu@users.noreply.github.com>
2023-06-10 23:41:51 -0400

    Update CrossbodyBag.java

--- src/main/java/CrossbodyBag.java
+++ src/main/java/CrossbodyBag.java
@@ -72,7 +72,7 @@ public class CrossbodyBag extends Bag{
 
 	 @Override
     public String toString() {
-        return this.color + " Crossbody Bag with " + numberOfStraps + " straps (" + this.numberOfContents + " / " +
-                this.capacity + ")";
+        return super.color + " Crossbody Bag with " + numberOfStraps + " straps (" + super.numberOfContents + " / " +
+                super.capacity + ")";
     }
 }
